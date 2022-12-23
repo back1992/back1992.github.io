871295e312796c054ecbae4b0d3036af6e97ec5f
linmk <back1992@gmail.com>
2022-12-23 15:03:37 +0800

    Initial commit

---
+++ static/js/my.js
@@ -0,0 +1,24 @@
+// var txt = '风雨人生道，冷暖一来回。刚愎青春病，不阿老年时。难为高尚人，拒作违心亊。仰俯多无愧，坦然候归途。';
+// var txt_list = ['风雨人生道，冷暖一来回。','刚愎青春病，不阿老年时。','难为高尚人，拒作违心亊。','仰俯多无愧，坦然候归途。'];
+// var speed = 280;
+
+// function typeWriter(id, txt) {
+// console.log(id);
+// console.log(txt);
+//   var i = 0;
+//   if (i < txt.length) {
+//     document.getElementById(id).innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter(id, txt), speed);
+//   }
+// }
+
+
+// window.onload=function(){
+//     for (var i = 0; i < 4; i++) {
+//         let id = "poem-90-" + i;
+//         console.log(id);
+//         console.log(txt_list[i]);
+//         typeWriter(id, txt_list[i])
+//       }
+// }
